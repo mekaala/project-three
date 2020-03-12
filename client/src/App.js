d422469bdc61974b2ce793a7281f1839cd6b076b
mekaala <mekaala@gmail.com>
2020-03-12 10:43:02 -0400

    added home page, navbar, and routes to schemas

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Environments from './components/Environments'
+import Relaxes from './components/Relaxes'
+import Focuses from './components/Focuses'
+import { Link } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
-      {/* <Router>
+        <Router>
+        <Home/>
+        <NavBar/>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/environments" component={ Environments }/>
+          <Route exact path="/relaxes" component={ Relaxes }/>
+          <Route exact path="/focuses" component={ Focuses }/>
         </Switch>
-      </Router> */}
+      </Router>
     </div>
   );
 }
