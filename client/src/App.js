42aa0367a20a98a9b0129dbb49afb1fb33d6fd26
mekaala <mekaala@gmail.com>
2020-03-12 13:42:57 -0400

    added Single components

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Environments from './components/Environments'
-import Relaxes from './components/Relaxes'
 import Focuses from './components/Focuses'
+import Relaxes from './components/Relaxes'
+import SingleEnvironment from './components/SingleEnvironment'
+import SingleFocus from './components/SingleFocus'
+import SingleRelax from './components/SingleRelax'
 import { Link } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-        <Router>
+      <Router>
         <Home/>
         <NavBar/>
         <Switch>
           <Route exact path="/environments" component={ Environments }/>
-          <Route exact path="/relaxes" component={ Relaxes }/>
+          <Route path="/environments/:environmentId" component={ SingleEnvironment }/>
           <Route exact path="/focuses" component={ Focuses }/>
+          <Route path="/focuses/:focusId" component={ SingleFocus }/>
+          <Route exact path="/relaxes" component={ Relaxes }/>
+          <Route path="/relaxes/:relaxId" component={ SingleRelax }/>
         </Switch>
       </Router>
     </div>
