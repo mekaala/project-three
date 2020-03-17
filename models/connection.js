b47d0b3f4b74e63369c3d25d848f9cffa5541a10
mekaala <mekaala@gmail.com>
2020-03-17 10:23:54 -0400

    connections

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
-
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/mindify";
-
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
-
-module.exports = mongoose
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/mindify');
+}
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', () => {
+  console.log("Mongoose has connected to MongoDB!");
+});
