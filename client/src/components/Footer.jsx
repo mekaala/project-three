e780d01801cfcd62f1d2d76a6efd83ab504d4987
mekaala <mekaala@gmail.com>
2020-03-19 10:57:47 -0400

    changed about me

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -56,7 +56,7 @@ export default class About extends Component {
                             <div className="about-description">
                                 <h2>Mekaal Ahmad</h2>
                                 <p>I am currently attending General Assembly as a student of the Software Engineering Immersive program. With this program, I hope to develop an advanced set of skills to benefit others. I hope to master website creation, and craft many successful projects.<br/>
-                                Outside of General Assembly, I am a massive Nintendo fan who loves all their games, and a photographer who focuses on capturing the night sky, landscapes, and nature. I hope that with this game, I can display my capabilities in Software Engineering.</p>
+                                Outside of General Assembly, I am a massive Nintendo fan who loves all their games, and a photographer who focuses on capturing the night sky, landscapes, and nature. I hope that with this application, I can display my capabilities in Software Engineering.</p>
                             </div>
                         </div>
                     </div>
